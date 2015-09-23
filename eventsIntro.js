d0a324936ff2caf82bcab1bbf7042a91c42ed0f4
sowjanya <sowju19@gmail.com>
2015-09-22 23:26:17 -0700

    Added events Intro

---
+++ eventsIntro.js
@@ -0,0 +1,9 @@
+var EventEmitter = require('events').EventEmitter;
+var logger = new EventEmitter();
+
+logger.on('error', function(message) {
+  console.log('ERR: ' + message);
+});
+
+logger.emit('error', 'Broken Sink');
+logger.emit('error', 'locked out of house');
